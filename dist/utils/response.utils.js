6c931631cf30ca2584eb24af984fae83d34f5805
Vishnu Kumar <vermaji1891@gmail.com>
2024-02-05 18:56:32 +0530

    remove build

--- dist/utils/response.utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UnknownError = void 0;
-exports.UnknownError = {
-    data: {},
-    success: false,
-    error: "Unknown Error",
-    message: "Something went wrong!"
-};
